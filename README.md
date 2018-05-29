ccfba7b5e79d9d7c68d24b4e7d5a4d8287a0c977
Victor Le Pen <victor@yarilabs.com>
2018-05-29 17:15:11 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# test_app_l2
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'category_route.dart';
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
+      home: CategoryRoute(),
+    );
+  }
+}
+
+
